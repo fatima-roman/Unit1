bde42772596a995c7e3a79682373c72cc7233f0a
fatima.roman <fatima.roman@106-26.info2>
2025-09-22 11:49:07 +0200

    ex7

--- src/unit1/Exercise7.java
+++ src/unit1/Exercise7.java
@@ -1,14 +1,37 @@
 package unit1;
 
+import java.util.Scanner;
+
 public class Exercise7 {
 
 	public static void main(String[] args) {
+		//Creamos un nuevo Scanner
+		Scanner sc = new Scanner(System.in);
+		
 		// Create variables 
 		String nombre; 
 		String direccion;
 		String telefono; 
 		
+		//Pedimos los datos 
+		System.out.print("Introduce tu nombre: ");
+		nombre = sc.nextLine();
+		System.out.print("\nIntroduce tu dirección: ");
+		direccion = sc.nextLine();
+		System.out.print("\nIntroduce tu numero de teléfono: ");
+		telefono = sc.nextLine();
+		
+		
+		//Imprimimos los datos
+		System.out.println("Nombre: " + nombre);
+		System.out.println("Dirección: " + direccion);
+		System.out.println("Teléfono: " + telefono);
+		
 
+		
+		
+		
+		sc.close();
 	}
 
 }
