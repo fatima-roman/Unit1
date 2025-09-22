d125a16bfa4fe95d10d892530b7007428720df3f
fatima.roman <fatima.roman@106-26.info2>
2025-09-22 10:00:08 +0200

    ex6

---
+++ src/unit1/Exercise06.java
@@ -0,0 +1,34 @@
+package unit1;
+
+import java.util.Scanner;
+
+public class Exercise06 {
+
+	public static void main(String[] args) {
+		//Creamos un nuevo Scanner
+		Scanner sc = new Scanner(System.in);
+				
+		//Creamos las variables
+		double numero1; 
+		double numero2;
+
+		//Pedimos los datos al usuario 
+		System.out.println("Introduce un número: ");
+		//Leemos el número introducido 
+		numero1 = sc.nextDouble(); 
+		
+		//Pedimos los datos al usuario 
+		System.out.println("Introduce otro número: ");
+		//Leemos el número introducido 
+		numero2 = sc.nextDouble(); 
+				
+		//Imprimimos el resultado de los datos introducidos introducido 
+		System.out.println("La suma es: " + (numero1+numero2) + "\nLa resta es: " +(numero1-numero2));
+		System.out.println("La multiplicación es: " +(numero1*numero2)+"\nLa división es: " +(numero1+numero2));
+		
+		sc.close();
+
+	}
+
+
+}
