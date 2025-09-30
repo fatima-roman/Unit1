7a3614d2d03a45ca12c55b7f7d26b7d577bd74b0
fatima.roman <fatima.roman@106-26.info2>
2025-09-30 13:29:00 +0200

    ex1-2 Acepta el Reto

---
+++ src/aceptaelreto/Ej2.java
@@ -0,0 +1,35 @@
+package aceptaelreto;
+
+import java.util.Scanner;
+
+public class Ej2 {
+
+	public static void main(String[] args) {
+		// Create new Scanner
+		Scanner sc = new Scanner(System.in);
+		
+		//Create variables 
+		String colgadas = "colgadas"; 
+		String a; 
+		int dato; 
+		
+		//Ask the user for a number
+		System.out.print("Introduce el número de intentos: ");
+		dato = sc.nextInt();
+
+		//calculate which direction it is  
+		for (int i = 0 ; i <dato; i++) {
+			System.out.print("Introduce el nombre: ");
+			a = sc.next();
+			a = a.toLowerCase();
+			a = a.equals(colgadas) ? "Bien" : "Mal";
+			//Print result 
+			System.out.println(a);
+		}
+		
+		
+		//Close Scanner
+		sc.close();
+	}
+
+}
