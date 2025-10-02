2155a4b95a43d614bad0c52d62a59d9a44741df0
fatima.roman <fatima.roman@106-26.info2>
2025-10-02 14:25:47 +0200

    ex2

--- src/aceptaelreto/Ej2.java
+++ src/aceptaelreto/Ej2.java
@@ -9,18 +9,20 @@ public class Ej2 {
 		Scanner sc = new Scanner(System.in);
 		
 		//Create variables 
-		String a; 
 		int dato; 
 		
-		//Ask the user for a number
-		System.out.print("Introduce el número de la calle: ");
+		//Ask the user for a number to start the loop 
+		System.out.print("Introduce el número de la calle (0 para salir): ");
 		dato = sc.nextInt();
-
-		//calculate which direction it is  
-		a = (dato%2==0)? "DERECHA":"IZQUIERDA";
 		
-		//Print result 
-		System.out.println("Esta en el lado: " + a);
+		while (dato != 0){
+			//calculate which direction it is  and print it 
+			System.out.println((dato%2==0)? "Esta en la DERECHA":"Esta en la IZQUIERDA");
+			
+			//Ask the user for a number
+			System.out.print("Introduce el número de la calle (0 para salir): ");
+			dato = sc.nextInt();
+		}
 		
 		//Close Scanner
 		sc.close();
